047faaa076ea52fd2cb162bc892a34015425eb32
Saquib Khan <35995394+saqkhan1995@users.noreply.github.com>
2023-06-05 01:52:16 +0530

    feat: test

---
+++ disableSSLWebClient3.java
@@ -0,0 +1,44 @@
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import javax.net.ssl.*;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+public class MainClass {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, KeyManagementException {
+        WebClient webClient = WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(createHttpClient()))
+                .build();
+
+        // Use the webClient for further operations
+    }
+
+    private static HttpClient createHttpClient() throws NoSuchAlgorithmException, KeyManagementException {
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, getTrustManagers(), new SecureRandom());
+
+        return HttpClient.create()
+                .secure(ssl -> ssl.sslContext(sslContext));
+    }
+
+    private static TrustManager[] getTrustManagers() {
+        return new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                    }
+
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                    }
+                }
+        };
+    }
+}
